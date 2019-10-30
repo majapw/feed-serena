bcf9a33e1e21682ad851a6f5a2edfb4fe4bf25f7
Maja Wichrowska <majapw@gmail.com>
2019-10-30 16:10:38 -0600

    Build out Header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './feed-serena.png';
 import avocado from './images/avocado.png';
 import './App.css';
+
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h1>FEED SERENA</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
+        <Header />
         <div className="App-layer">
-          <img src={avocado} className="App-food" alt="avocado" />
+          {/* <img src={avocado} className="App-food" alt="avocado" /> */}
         </div>
       </div>
     );
