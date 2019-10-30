5b38969907e4036989d2d58d79a89ee9352eee39
Maja Wichrowska <majapw@gmail.com>
2019-10-30 16:42:08 -0600

    Add Game file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Header from './Header';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Game.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Game.css';
+
+import avocado from './images/avocado.png';
+
+
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className="Game">
+      </div>
+    );
+  }
+}
+
+export default Game;
