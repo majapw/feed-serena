a97873e22bef1c2d399a109a3cda1ff4992fe810
Maja Wichrowska <majapw@gmail.com>
2019-10-31 00:22:44 -0600

    Add more images

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,10 +1,25 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import './Food.css';
 
 import avocado from './images/avocado.png';
 import orange from './images/orange.png';
 import chocochip from './images/choco-chip-cookie.png';
+import apple from './images/apple.png';
+import cereal from './images/cereal.png';
+import doubleSandwich from './images/double-sandwich.png';
+import iceCreamCone from './images/ice-cream-cone.png';
+import pancakes from './images/pancakes.png';
+import waffles from './images/waffles.png';
+import eggsAndBacon from './images/eggs-and-bacon.png';
+import oatmeal from './images/oatmeal.png';
+import popsicle from './images/popsicle.png';
+import spaghetti from './images/spaghetti.png';
+import burgersAndFries from './images/burgers-and-fries.png';
+import croissant from './images/croissant.png';
+import grapefruit from './images/grapefruit-juice';
+import sandwich from './images/sandwich.png';
+import sushi from './images/sushi.png';
 
 export const FOOD_HEIGHT = 100;
 export const FOODS = {
